3081cb38db85b8f20821b2ea13f37b89cc49644e
rihan nonim <rihan.nonim@gmail.com>
2020-11-30 15:06:16 +0700

    Add project files.

---
+++ WpfApp1/views/LoginPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp1.views
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+
+       
+        public LoginPage()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void TextBlock_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("oh hello");
+        }
+
+       
+
+       
+        MainWindow MainWindow { get => Application.Current.MainWindow as MainWindow; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.mainFrame.Navigate(new Uri("/views/DashboardPage.xaml", UriKind.RelativeOrAbsolute));
+            
+        }
+    }
+}
